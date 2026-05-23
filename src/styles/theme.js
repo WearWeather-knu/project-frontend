ebb91f7e4b7c809ddd729e60cc61400e7c49648b
최연우 <florau@knu.ac.kr>
2026-05-23 13:21:00 +0900

    feature: add theme main color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,31 @@
 const theme = {
   colors: {
-    primary: '',
-    background: '',
-    text: '',
-    border: '',
+    text: '#43474F',
+    background: '#ffffff',
+    border: '#E5E7EB',
+
+    seasons: {
+      spring: {
+        primary: '#66B78B',
+        background: 'linear-gradient(27deg, #FFFFFF 13%, rgba(171, 245, 159, 0.49) 63%), rgba(217, 240, 104, 0.2) 89%)',
+        text: '#448662',
+      },
+      summer: {
+        primary: '#91D6E9',
+        background: 'linear-gradient(27deg, #ffffff 13%, rgba(159, 224, 245, 0.3) 67%, rgba(240, 217, 104, 0.1) 89%)',
+        text: '#81C6D9',
+      },
+      autumn: {
+        primary: '#FF8E8F',
+        background: 'linear-gradient(27deg, #FFFFFF 13%, rgba(245, 159, 169, 0.49) 63%, rgba(240, 140, 104, 0.2) 89%)',
+        text: '#EE7576',
+      },
+      winter: {
+        primary: '#31326F',
+        background: 'linear-gradient(27deg, #FFFFFF 13%, rgba(182, 164, 215, 0.49) 63%, rgba(177, 206, 255, 0.2) 89%)',
+        text: '#31326F',
+      },
+    },
   },
   heights: {
     header: '56px',
