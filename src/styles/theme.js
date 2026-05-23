6c3f6b298a40c39c670a33b36c7ef2cd33d6b581
최연우 <florau@knu.ac.kr>
2026-05-23 14:19:10 +0900

    refactor: fixed background color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,22 +7,22 @@ const theme = {
     seasons: {
       spring: {
         primary: '#66B78B',
-        background: 'linear-gradient(27deg, #FFFFFF 13%, rgba(171, 245, 159, 0.49) 63%, rgba(217, 240, 104, 0.2) 89%)',
+        background: 'linear-gradient(203deg, #FFFFFF 35%, rgba(171, 245, 159, 0.3) 57%, rgba(217, 240, 104, 0.2) 82%)',
         text: '#448662',
       },
       summer: {
         primary: '#91D6E9',
-        background: 'linear-gradient(27deg, #ffffff 13%, rgba(159, 224, 245, 0.3) 67%, rgba(240, 217, 104, 0.1) 89%)',
+        background: 'linear-gradient(203deg, #ffffff 35%, rgba(159, 224, 245, 0.3) 58%, rgba(240, 217, 104, 0.1) 89%)',
         text: '#81C6D9',
       },
       autumn: {
         primary: '#FF8E8F',
-        background: 'linear-gradient(27deg, #FFFFFF 13%, rgba(245, 159, 169, 0.49) 63%, rgba(240, 140, 104, 0.2) 89%)',
+        background: 'linear-gradient(203deg, #FFFFFF 35%, rgba(245, 159, 169, 0.4) 56%, rgba(240, 179, 104, 0.2) 77%)',
         text: '#EE7576',
       },
       winter: {
         primary: '#31326F',
-        background: 'linear-gradient(27deg, #FFFFFF 13%, rgba(182, 164, 215, 0.49) 63%, rgba(177, 206, 255, 0.2) 89%)',
+        background: 'linear-gradient(203deg, #FFFFFF 35%, rgba(182, 164, 215, 0.49) 60%, rgba(177, 206, 255, 0.2) 78%)',
         text: '#31326F',
       },
     },
