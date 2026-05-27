d3a47be40a02593ab1c8bf25263b0d32219c3305
hotpringles <jhchoi3669@gmail.com>
2026-05-28 01:03:35 +0900

    fix: 토큰 정보 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
   useEffect(() => {
     supabase.auth.getSession().then(({ data }) => {
       setSession(data.session);
-      console.log(data.session);
       // console.log(data.session?.access_token);
       setLoading(false);
     });
