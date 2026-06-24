36ef1656c5d3d590569582cc0d78c3b849e4cf17
최연우 <florau@knu.ac.kr>
2026-06-24 18:01:19 +0900

    added app icon

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/icons/app%20icon.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/icon-512.png',
+            src: '/icons/app%20icon.png',
             sizes: '512x512',
             type: 'image/png',
           },
